e50f853a37be95b13e42a5decde193f39b1c9344
ronnie.gandhi <ronnie.gandhi@phonepe.com>
2025-06-28 22:42:26 +0530

    Adding more structure

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -39,7 +39,7 @@ export default [
   },
   {
     path: '/',
-    component: ComponentCreator('/', '24b'),
+    component: ComponentCreator('/', '452'),
     routes: [
       {
         path: '/',
@@ -48,44 +48,301 @@ export default [
         sidebar: "mainSidebar"
       },
       {
-        path: '/concept/loop',
-        component: ComponentCreator('/concept/loop', '1b3'),
+        path: '/business/',
+        component: ComponentCreator('/business/', '149'),
         exact: true,
         sidebar: "mainSidebar"
       },
       {
-        path: '/go-to-market/strategy',
-        component: ComponentCreator('/go-to-market/strategy', 'd7d'),
+        path: '/business/gtm/',
+        component: ComponentCreator('/business/gtm/', 'ffd'),
         exact: true,
         sidebar: "mainSidebar"
       },
       {
-        path: '/monetization/',
-        component: ComponentCreator('/monetization/', '2d4'),
+        path: '/business/gtm/final',
+        component: ComponentCreator('/business/gtm/final', '4bd'),
         exact: true,
         sidebar: "mainSidebar"
       },
       {
-        path: '/pitch/founder',
-        component: ComponentCreator('/pitch/founder', 'bd5'),
+        path: '/business/gtm/phase0',
+        component: ComponentCreator('/business/gtm/phase0', '903'),
         exact: true,
         sidebar: "mainSidebar"
       },
       {
-        path: '/product/cloning',
-        component: ComponentCreator('/product/cloning', '4b8'),
+        path: '/business/gtm/phase1',
+        component: ComponentCreator('/business/gtm/phase1', '31b'),
         exact: true,
         sidebar: "mainSidebar"
       },
       {
-        path: '/tech/',
-        component: ComponentCreator('/tech/', 'e4b'),
+        path: '/business/gtm/phase2',
+        component: ComponentCreator('/business/gtm/phase2', '637'),
         exact: true,
         sidebar: "mainSidebar"
       },
       {
-        path: '/tech/architecture',
-        component: ComponentCreator('/tech/architecture', 'ae5'),
+        path: '/business/gtm/phase3',
+        component: ComponentCreator('/business/gtm/phase3', '8d7'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/business/monetization/',
+        component: ComponentCreator('/business/monetization/', '04b'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/business/monetization/ads',
+        component: ComponentCreator('/business/monetization/ads', 'a17'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/business/monetization/creator-brand-deals-commissions',
+        component: ComponentCreator('/business/monetization/creator-brand-deals-commissions', 'dc4'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/business/monetization/developer-app-commissions',
+        component: ComponentCreator('/business/monetization/developer-app-commissions', '64f'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/business/monetization/geofencing',
+        component: ComponentCreator('/business/monetization/geofencing', '76b'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/business/monetization/others',
+        component: ComponentCreator('/business/monetization/others', 'a49'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/business/tam',
+        component: ComponentCreator('/business/tam', '4c9'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/competition/',
+        component: ComponentCreator('/competition/', 'fb0'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/competition/apple',
+        component: ComponentCreator('/competition/apple', '404'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/competition/bytedance',
+        component: ComponentCreator('/competition/bytedance', '136'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/competition/google',
+        component: ComponentCreator('/competition/google', '236'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/competition/meta',
+        component: ComponentCreator('/competition/meta', 'c24'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/competition/niantic-labs',
+        component: ComponentCreator('/competition/niantic-labs', '8df'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/competition/snap',
+        component: ComponentCreator('/competition/snap', '88b'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/concept/features/',
+        component: ComponentCreator('/concept/features/', '3c8'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/concept/features/aichat',
+        component: ComponentCreator('/concept/features/aichat', '276'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/concept/features/clone',
+        component: ComponentCreator('/concept/features/clone', 'eda'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/concept/features/explore',
+        component: ComponentCreator('/concept/features/explore', 'a43'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/concept/features/post',
+        component: ComponentCreator('/concept/features/post', 'aa8'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/concept/impact',
+        component: ComponentCreator('/concept/impact', '37c'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/concept/what',
+        component: ComponentCreator('/concept/what', 'e2a'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/core-technology/',
+        component: ComponentCreator('/core-technology/', 'eb6'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/core-technology/ar-anchoring',
+        component: ComponentCreator('/core-technology/ar-anchoring', 'c70'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/core-technology/edge-computation',
+        component: ComponentCreator('/core-technology/edge-computation', '7d0'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/core-technology/generative-ai',
+        component: ComponentCreator('/core-technology/generative-ai', '453'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/core-technology/reality-graph',
+        component: ComponentCreator('/core-technology/reality-graph', '53d'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/extras/concept/loop',
+        component: ComponentCreator('/extras/concept/loop', '342'),
+        exact: true
+      },
+      {
+        path: '/extras/go-to-market/strategy',
+        component: ComponentCreator('/extras/go-to-market/strategy', 'b07'),
+        exact: true
+      },
+      {
+        path: '/extras/monetization/',
+        component: ComponentCreator('/extras/monetization/', 'e8e'),
+        exact: true
+      },
+      {
+        path: '/extras/pitch/founder',
+        component: ComponentCreator('/extras/pitch/founder', '3b5'),
+        exact: true
+      },
+      {
+        path: '/extras/product/cloning',
+        component: ComponentCreator('/extras/product/cloning', '5f0'),
+        exact: true
+      },
+      {
+        path: '/extras/tech/',
+        component: ComponentCreator('/extras/tech/', '1dd'),
+        exact: true
+      },
+      {
+        path: '/extras/tech/architecture',
+        component: ComponentCreator('/extras/tech/architecture', '9ca'),
+        exact: true
+      },
+      {
+        path: '/psychology/',
+        component: ComponentCreator('/psychology/', 'da1'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/psychology/habit-loop',
+        component: ComponentCreator('/psychology/habit-loop', '72d'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/psychology/hook-like-slot-machine',
+        component: ComponentCreator('/psychology/hook-like-slot-machine', 'efb'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/psychology/why-tiktok-works',
+        component: ComponentCreator('/psychology/why-tiktok-works', '9e1'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/why/',
+        component: ComponentCreator('/why/', 'e7f'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/why/history',
+        component: ComponentCreator('/why/history', 'c97'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/why/key-insights',
+        component: ComponentCreator('/why/key-insights', '8bb'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/why/me',
+        component: ComponentCreator('/why/me', '11d'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/why/now',
+        component: ComponentCreator('/why/now', '932'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/why/why-for-consumers',
+        component: ComponentCreator('/why/why-for-consumers', '381'),
+        exact: true,
+        sidebar: "mainSidebar"
+      },
+      {
+        path: '/why/why-for-creators',
+        component: ComponentCreator('/why/why-for-creators', '989'),
         exact: true,
         sidebar: "mainSidebar"
       }
--- sidebars.js
+++ sidebars.js
@@ -1,43 +1,107 @@
 module.exports = {
-    mainSidebar: [
-      {
-        type: 'doc',
-        id: 'index',
-        label: 'Introduction',
-      },
-      {
-        type: 'category',
-        label: 'Concept',
-        items: ['concept/loop'],
-      },
-      {
-        type: 'category',
-        label: 'Go To Market',
-        items: ['go-to-market/strategy'],
-      },
-      {
-        type: 'category',
-        label: 'Monetization',
-        items: ['monetization/index'],
-      },
-      {
-        type: 'category',
-        label: 'About Founder',
-        items: ['pitch/founder'],
-      },
-      {
-        type: 'category',
-        label: 'Product',
-        items: ['product/cloning'],
-      },
-      {
-        type: 'category',
-        label: 'Tech',
-        items: [
-          'tech/index',
-          'tech/architecture',
-        ],
-      },
-    ],
-  };
-  
+  mainSidebar: [
+    {
+      type: 'doc',
+      id: 'index',
+      label: 'Introduction',
+    },
+    {
+      type: 'category',
+      label: 'Concept (What?)',
+      items: [
+        'concept/what',
+        {
+          type: 'category',
+          label: 'Features',
+          link: { type: 'doc', id: 'concept/features/index' },
+          items: [
+            'concept/features/post',
+            'concept/features/clone',
+            'concept/features/explore',
+            'concept/features/aichat',
+          ],
+        },
+        'concept/impact',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Why?',
+      link: { type: 'doc', id: 'why/index' },
+      items: [
+        'why/me',
+        'why/now',
+        'why/why-for-creators',
+        'why/why-for-consumers',
+        'why/key-insights',
+        'why/history',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Technology',
+      link: { type: 'doc', id: 'core-technology/index' },
+      items: [
+        'core-technology/ar-anchoring',
+        'core-technology/reality-graph',
+        'core-technology/generative-ai',
+        'core-technology/edge-computation',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Psychology',
+      link: { type: 'doc', id: 'psychology/index' },
+      items: [
+        'psychology/habit-loop',
+        'psychology/why-tiktok-works',
+        'psychology/hook-like-slot-machine',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Business',
+      link: { type: 'doc', id: 'business/index' },
+      items: [
+        'business/tam',
+        {
+          type: 'category',
+          label: 'Monetization Avenues',
+          link: { type: 'doc', id: 'business/monetization/index' },
+          items: [
+            'business/monetization/ads',
+            'business/monetization/creator-brand-deals-commissions',
+            'business/monetization/geofencing',
+            'business/monetization/developer-app-commissions',
+            'business/monetization/others',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'GTM & Growth Strategy',
+          link: { type: 'doc', id: 'business/gtm/index' },
+          items: [
+            'business/gtm/phase0',
+            'business/gtm/phase1',
+            'business/gtm/phase2',
+            'business/gtm/phase3',
+            'business/gtm/final',
+          ],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Competition',
+      link: { type: 'doc', id: 'competition/index' },
+      items: [
+        'competition/meta',
+        'competition/bytedance',
+        'competition/snap',
+        'competition/google',
+        'competition/niantic-labs',
+        'competition/apple',
+      ],
+    },
+  ],
+};
